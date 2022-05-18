f220b90a9edc21ff65e63ae510e8800874c9e4b8
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2022-05-17 21:03:59 -0500

    Add files via upload

---
+++ Pen.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("Pen.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults)
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
+}
